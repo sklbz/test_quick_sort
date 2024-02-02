04ea7ca1f1495b1b9886375b81f1f069b072fa6e
sklbz <sokolbozanic@gmail.com>
2024-02-02 20:57:23 +0100

    added merge sort start

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+mod quick_sort;
+mod merge_sort;
+use quick_sort::quick_sort;
+use merge_sort::merge_sort;
+
 fn main() {
+    let input = vec![0,2,1];
     println!("Hello, world!");
+    quick_sort(input);
+    merge_sort();
 }
---
+++ src/quick_sort.rs
@@ -0,0 +1,8 @@
+pub fn quick_sort(input: Vec<u32>) -> Vec<u32>{
+    let pivot: usize = input.len();
+    let result = 
+    for i in input.len() {
+
+    }
+    return input;
+}
